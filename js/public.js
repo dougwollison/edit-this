5c9fd1f4deaa40964f038b898bccd3d505dc037f
Doug Wollison <doug@wollison.net>
2016-10-12 12:09:29 -0400

    Fleshed out javascript functionality

--- js/public.js
+++ js/public.js
@@ -1,4 +1,22 @@
 /* globals editthisL10n */
 jQuery( function( $ ) {
-	// to be written
+	var visible = $( 'body' ).hasClass( 'editthis-visible' );
+	$( '#wp-admin-bar-editthis-toggle a' ).click( function( e ) {
+		e.preventDefault();
+
+		// Toggle visible status
+		visible = ! visible;
+		// Update text
+		$( this ).text( visible ? editthisL10n.hide : editthisL10n.show );
+		// Change body class
+		$( 'body' ).toggleClass( 'editthis-visible', visible ).toggleClass( 'editthis-hidden', ! visible );
+
+		$.ajax( {
+			url: editthisL10n.ajax_url,
+			data: {
+				action: 'editthis_toggle',
+				default: hidden ? 0 : 1,
+			}
+		} );
+	} );
 } );
