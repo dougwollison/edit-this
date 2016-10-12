e2074a6da4127c8422dace7c1a45a628fb4aa873
Doug Wollison <doug@wollison.net>
2016-10-12 11:59:56 -0400

    Fixed unedited placeholders, added ajax_url, body class and admin bar filters

--- includes/class-editthis-frontend.php
+++ includes/class-editthis-frontend.php
@@ -41,6 +41,10 @@ final class Frontend extends Handler {
 
 		// Script/Style Enqueues
 		self::add_action( 'enqueue_scripts', 'enqueue_assets' );
+
+		// Output Editing
+		self::add_filter( 'body_class', 'add_visibility_class', 999, 1 );
+		self::add_action( 'admin_bar_menu', 'add_toggle_button', 999, 1 );
 	}
 
 	// =========================
@@ -68,14 +72,50 @@ final class Frontend extends Handler {
 	 */
 	public static function enqueue_assets(){
 		// Admin styling
-		wp_enqueue_style( 'editthis', plugins_url( 'css/public.css', SLUG_PLUGIN_FILE ), '1.0.0', 'screen' );
+		wp_enqueue_style( 'editthis', plugins_url( 'css/public.css', EDITTHIS_PLUGIN_FILE ), '1.0.0', 'screen' );
 
 		// Admin javascript
-		wp_enqueue_script( 'editthis-js', plugins_url( 'js/public.js', SLUG_PLUGIN_FILE ), array(), '1.0.0' );
+		wp_enqueue_script( 'editthis-js', plugins_url( 'js/public.js', EDITTHIS_PLUGIN_FILE ), array(), '1.0.0' );
 
 		// Localize the javascript
-		wp_localize_script( 'editthis-js', 'pluginnameL10n', array(
-			// to be written
+		wp_localize_script( 'editthis-js', 'editthisL10n', array(
+			'ajax_url' => admin_url( 'admin-ajax.php' ),
+		) );
+	}
+
+	// =========================
+	// ! Output Editing
+	// =========================
+
+	/**
+	 * Add the editthis-visible/hidden class to the body.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $classes The classes for the body.
+	 *
+	 * @return array The updated classes list.
+	 */
+	public static function add_visibility_class( $classes ) {
+		$classes[] = 'editthis-' . get_default_visibility();
+
+		return $classes;
+	}
+
+	/**
+	 * Add the toggle button to the admin bar.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param \WP_Admin_Bar $admin_bar The admin bar to edit.
+	 */
+	public static function add_toggle_button( \WP_Admin_Bar $admin_bar ) {
+		$visible = get_default_visibility() == 'visible';
+
+		$admin_bar->add_node( array(
+			'id'    => 'editthis-toggle',
+			'title' => $visible ? __( 'Hide Edit Buttons', 'editthis' ) : __( 'Show Edit Buttons', 'editthis' ),
+			'href'  => '#',
 		) );
 	}
 }
